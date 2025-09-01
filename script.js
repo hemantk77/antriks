93ace8d9546c8ed005aaa9e36fb117ce8d30797a
hemantk77 <hemant11uk@gmail.com>
2025-09-01 17:59:36 +0200

    Typing animation for the intro done

--- script.js
+++ script.js
@@ -12,3 +12,18 @@
           window.scrollTo({ top: 0, behavior: 'smooth' });
         });
       })();
+
+const text = "Humanity's gaze has always been fixed on the cosmos, dreaming of the worlds beyond our own. Today, that dream unfolds in real-time with every launch that lights up the sky. Antriks is your front-row seat to this grand endeavor, charting the missions that define our future. Witness the next chapter begin.";
+const typingE1 = document.getElementById("typing");
+
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    typingE1.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 25);
+  }
+}
+
+typeWriter();
